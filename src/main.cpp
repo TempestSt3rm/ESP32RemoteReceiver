579953f5fc4445c666306766a7ab993c07b2b932
Jeff <jeffreywjlim@gmail.com>
2024-10-17 20:21:06 -0400

    changed to 5 pin latch

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,43 @@
 #include <esp_now.h>
 #include <WiFi.h>
 
-int LED_PIN = 2;  // Assuming GPIO 2 is connected to an LED
-bool ledState = LOW;  // Variable to track the LED state
+//5 Outputs represented as 5 leds B,G,R,B,G
+int LED0_PIN = 21; 
+int LED1_PIN = 22; 
+int LED2_PIN = 19; 
+int LED3_PIN = 23; 
+int LED4_PIN = 18; 
+
+bool LED0_state = LOW; 
+bool LED1_state = LOW; 
+bool LED2_state = LOW; 
+bool LED3_state = LOW; 
+bool LED4_state = LOW;
+
+bool* LEDs_State[5] = {
+  &LED0_state,
+  &LED1_state,
+  &LED2_state,
+  &LED3_state,
+  &LED4_state
+};
+
+int LEDBuffer0[2] = {0,0};
+int LEDBuffer1[2] = {0,0};
+int LEDBuffer2[2] = {0,0};
+int LEDBuffer3[2] = {0,0};
+int LEDBuffer4[2] = {0,0};
+
+int* LEDsBuffer[5] = {LEDBuffer0,LEDBuffer1,LEDBuffer2,LEDBuffer3,LEDBuffer4};
+int risingEdge[2] = {1,0};
 
 // Structure to receive data
 typedef struct struct_message {
-  char message[32];
-  int button_state;
+  int button_state0;
+  int button_state1;
+  int button_state2;
+  int button_state3;
+  int button_state4;
 } struct_message;
 
 // Create a struct_message to hold the received data
@@ -25,13 +55,41 @@ void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   dataReceived = true;
 }
 
+
+bool compareArrays(int a[], int b[], int size) {
+  for (int i = 0; i < size; i++) {
+    if (a[i] != b[i]) {
+      return false;  // Return false if any elements differ
+    }
+  }
+  return true;  // Return true if all elements are equal
+}
+
+void shiftArrays(int* arrays[], int numArrays, int arraySize, int inputs[]) {
+  // Iterate through each array
+  for (int i = 0; i < numArrays; i++) {
+    // Shift the elements in the current array to the right, leaving the 0th index open for the new one
+    for (int j = arraySize - 1; j > 0; j--) {
+      arrays[i][j] = arrays[i][j - 1];
+    }
+    // Set the new input value at index 0
+    arrays[i][0] = inputs[i];
+  }
+}
+
+
+
 void setup() {
   // Initialize Serial Monitor
   Serial.begin(115200);
   Serial.println("Starting Receiver");
 
   // Set LED pin as output
-  pinMode(LED_PIN, OUTPUT);
+  pinMode(LED0_PIN, OUTPUT);
+  pinMode(LED1_PIN, OUTPUT);
+  pinMode(LED2_PIN, OUTPUT);
+  pinMode(LED3_PIN, OUTPUT);
+  pinMode(LED4_PIN, OUTPUT);
 
   // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
@@ -51,21 +109,28 @@ void loop() {
   if (dataReceived) {
     // Reset the flag
     dataReceived = false;
+ 
+    int inputArray[5] = {
+      myData.button_state0,
+      myData.button_state1,
+      myData.button_state2,
+      myData.button_state3,
+      myData.button_state4,
+    };
 
-    // Print the received data
-    Serial.print("Received message: ");
-    Serial.println(myData.message);
-    Serial.print("Button state: ");
-    Serial.println(myData.button_state);
+    shiftArrays(LEDsBuffer,5,2,inputArray);
 
     // Set the LED state based on the received button state
-    if (myData.button_state == 1) {
-      ledState = HIGH;  // Button pressed, turn LED on
-    } else {
-      ledState = LOW;  // Button released, turn LED off
-    }
+    for (int i = 0; i<5; i++){
+      if(compareArrays(LEDsBuffer[i],risingEdge,2)) {
+        *LEDs_State[i] = ! *LEDs_State[i]; 
+      }
+    };
 
-    // Apply the LED state (this happens outside the interrupt context)
-    digitalWrite(LED_PIN, ledState);
+    digitalWrite(LED0_PIN,LED0_state);
+    digitalWrite(LED1_PIN,LED1_state);
+    digitalWrite(LED2_PIN,LED2_state);
+    digitalWrite(LED3_PIN,LED3_state);
+    digitalWrite(LED4_PIN,LED4_state);
   }
 }
