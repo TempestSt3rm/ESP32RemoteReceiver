fb16b8517a164588bdf8852514548c64872d0687
Jeff <jeffreywjlim@gmail.com>
2024-12-09 20:38:56 -0500

    final prototype

--- src/main.cpp
+++ src/main.cpp
@@ -1,95 +1,70 @@
 #include <esp_now.h>
 #include <WiFi.h>
 
-//5 Outputs represented as 5 leds B,G,R,B,G
-int LED0_PIN = 21; 
-int LED1_PIN = 22; 
-int LED2_PIN = 19; 
-int LED3_PIN = 23; 
-int LED4_PIN = 18; 
+// Define LED Pins
+int LED0_PIN = 18; 
+int LED1_PIN = 19; 
+
+int LED0_confirm_Pin = 32; 
+int LED1_confirm_Pin = 25; 
 
 bool LED0_state = LOW; 
 bool LED1_state = LOW; 
-bool LED2_state = LOW; 
-bool LED3_state = LOW; 
-bool LED4_state = LOW;
 
-bool* LEDs_State[5] = {
+bool* LEDs_State[2] = {
   &LED0_state,
-  &LED1_state,
-  &LED2_state,
-  &LED3_state,
-  &LED4_state
+  &LED1_state
 };
 
-int LEDBuffer0[2] = {0,0};
-int LEDBuffer1[2] = {0,0};
-int LEDBuffer2[2] = {0,0};
-int LEDBuffer3[2] = {0,0};
-int LEDBuffer4[2] = {0,0};
+int LEDBuffer0[2] = {0, 0};
+int LEDBuffer1[2] = {0, 0};
 
-int* LEDsBuffer[5] = {LEDBuffer0,LEDBuffer1,LEDBuffer2,LEDBuffer3,LEDBuffer4};
-int risingEdge[2] = {1,0};
+int* LEDsBuffer[2] = {LEDBuffer0, LEDBuffer1};
+int risingEdge[2] = {1, 0};
 
-// Structure to receive data
-typedef struct struct_message {
+// Struct for transmitted and received messages
+typedef struct struct_transmission_message {
   int transmitted_state0;
   int transmitted_state1;
-  int transmitted_state2;
-  int transmitted_state3;
-  int transmitted_state4;
-} struct_message;
+} struct_transmission_message;
 
 // Create a struct_message to hold the received data
-struct_message myData;
+struct_transmission_message myData;
+
+// Create a struct_message to hold the constant transmitted data
+struct_transmission_message confirmMessage;
 
 // Flag to indicate that new data has been received
 volatile bool dataReceived = false;
 
-// callback function that will be executed when data is received
-void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+// MAC address of the other ESP32 (the transmitter)
+// mac address of the transmitter 30:83:98:ee:4e:38
+uint8_t broadcastAddress[] = {0x30, 0x83, 0x98, 0xee, 0x4e, 0x38};
+
+// Callback function to handle received data
+void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len) {
   // Copy the received data into the myData structure
-  memcpy(&myData, incomingData, sizeof(struct_message));
+  memcpy(&myData, incomingData, sizeof(struct_transmission_message));
   // Set flag to true to indicate data was received (this will be handled in loop)
   dataReceived = true;
 }
 
-
-bool compareArrays(int a[], int b[], int size) {
-  for (int i = 0; i < size; i++) {
-    if (a[i] != b[i]) {
-      return false;  // Return false if any elements differ
-    }
-  }
-  return true;  // Return true if all elements are equal
+// Callback function to handle transmission status
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
-
-void shiftArrays(int* arrays[], int numArrays, int arraySize, int inputs[]) {
-  // Iterate through each array
-  for (int i = 0; i < numArrays; i++) {
-    // Shift the elements in the current array to the right, leaving the 0th index open for the new one
-    for (int j = arraySize - 1; j > 0; j--) {
-      arrays[i][j] = arrays[i][j - 1];
-    }
-    // Set the new input value at index 0
-    arrays[i][0] = inputs[i];
-  }
-}
-
-
 
 void setup() {
   // Initialize Serial Monitor
   Serial.begin(115200);
-  Serial.println("Starting Receiver 222");
-  // WiFi.begin("dummySSID", "dummyPassword", 1);
+  Serial.println("Starting Receiver with Transmitter");
 
-  // Set LED pin as output
+  // Set LED pins as outputs
   pinMode(LED0_PIN, OUTPUT);
   pinMode(LED1_PIN, OUTPUT);
-  pinMode(LED2_PIN, OUTPUT);
-  pinMode(LED3_PIN, OUTPUT);
-  pinMode(LED4_PIN, OUTPUT);
+  pinMode(LED0_confirm_Pin, INPUT);
+  pinMode(LED1_confirm_Pin, INPUT);
 
   // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
@@ -101,7 +76,25 @@ void setup() {
   }
 
   // Register the callback function to handle received ESP-NOW data
-  esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
+  esp_now_register_recv_cb(OnDataRecv);
+
+  // Register the callback function to handle transmission status
+  esp_now_register_send_cb(OnDataSent);
+
+  // Add the peer (sender device)
+  esp_now_peer_info_t peerInfo;
+  memset(&peerInfo, 0, sizeof(peerInfo)); 
+  memcpy(peerInfo.peer_addr, broadcastAddress, sizeof(broadcastAddress));
+  peerInfo.channel = 1;
+  peerInfo.encrypt = false;
+
+  if (!esp_now_is_peer_exist(broadcastAddress)) {
+    esp_now_del_peer(broadcastAddress); // Ensure no duplicate peer exists
+    if (esp_now_add_peer(&peerInfo) != ESP_OK) {
+      Serial.println("Failed to add peer");
+      return;
+    }
+  }
 }
 
 void loop() {
@@ -109,35 +102,30 @@ void loop() {
   if (dataReceived) {
     // Reset the flag
     dataReceived = false;
- 
-    // int inputArray[5] = {
-    //   myData.button_state0,
-    //   myData.button_state1,
-    //   myData.button_state2,
-    //   myData.button_state3,
-    //   myData.button_state4,
-    // };
 
+    // Print received data
     Serial.print("Data received button state:");
     Serial.print(myData.transmitted_state0);
     Serial.print(myData.transmitted_state1);
-    Serial.print(myData.transmitted_state2);
-    Serial.print(myData.transmitted_state3);
-    Serial.println(myData.transmitted_state4);
 
-    // shiftArrays(LEDsBuffer,5,2,inputArray);
-
-    // Set the LED state based on the received button state
-    // for (int i = 0; i<5; i++){
-    //   if(compareArrays(LEDsBuffer[i],risingEdge,2)) {
-    //     *LEDs_State[i] = ! *LEDs_State[i]; 
-    //   }
-    // };
-
-    digitalWrite(LED0_PIN,myData.transmitted_state0);
-    digitalWrite(LED1_PIN,myData.transmitted_state1);
-    digitalWrite(LED2_PIN,myData.transmitted_state2);
-    digitalWrite(LED3_PIN,myData.transmitted_state3);
-    digitalWrite(LED4_PIN,myData.transmitted_state4);
+    // Update LED states
+    digitalWrite(LED0_PIN, myData.transmitted_state0);
+    digitalWrite(LED1_PIN, myData.transmitted_state1);
   }
+  confirmMessage.transmitted_state0 = digitalRead(LED0_confirm_Pin);
+  confirmMessage.transmitted_state1 = digitalRead(LED1_confirm_Pin);
+
+  Serial.println("confirmed states ");
+  Serial.print(confirmMessage.transmitted_state0);
+  Serial.println(confirmMessage.transmitted_state1);
+
+  // Send a constant message back to the transmitter
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &confirmMessage, sizeof(confirmMessage));
+  if (result == ESP_OK) {
+    Serial.println("Confirm message sent successfully");
+  } else {
+    Serial.println("Error sending constant message");
+  }
+
+  delay(200); 
 }
